bc3cd4c887e89abaa9529aa7a389bee9feb11064
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2023-04-28 15:17:22 +0100

    implement generate PdfReport method

--- App-2-Flatmates-Bill/main.py
+++ App-2-Flatmates-Bill/main.py
@@ -1,3 +1,6 @@
+from fpdf import FPDF
+
+
 class Bill:
     """
     Creates a bill object
@@ -27,7 +30,9 @@ class Flatmate:
 
 class PdfReport:
     """
-    Creates a Pdf file that contains flatmate data(name, due amount for period)
+    Creates a Pdf file that contains flatmate data(
+        name, due amount for period
+    )
     and the period of bill
     """
 
@@ -35,10 +40,37 @@ class PdfReport:
         self.filename = filename
 
     def generate(self, flatmate1, flatmate2, bill):
-        pass
+        # Creates pdf object
+        pdf = FPDF(orientation='P', unit='pt', format='A4')
+        pdf.add_page()
+
+        # Add text
+        pdf.set_font(family='Times', size=24, style='B')
+        pdf.cell(
+            w=0, h=80, txt='Flatmate Bill', align='C', ln=1
+        )
+        pdf.cell(w=200, h=40, txt=f'Period: {bill.period}', ln=1)
+        pdf.cell(
+            w=175, h=40,
+            txt=f'{flatmate1.name}: {round(flatmate1.pays(bill, flatmate2), 2)}',
+            ln=1
+        )
+        pdf.cell(
+            w=175, h=40,
+            txt=f'{flatmate2.name}: {round(flatmate2.pays(bill, flatmate1), 2)}',
+            ln=1
+        )
+        pdf.cell(
+            w=175, h=40,
+            txt=f'Total: {round(float(bill.amount), 2)}',
+            ln=1
+        )
+
+        # Saves pdf
+        pdf.output(f"{self.filename}.pdf")
 
 
-bill = Bill(amount=120, period="March 2022")
+bill = Bill(amount=120, period="September 2022")
 john = Flatmate(name="John", days_in_house=20)
 marry = Flatmate(name="Marry", days_in_house=25)
 
@@ -48,3 +80,5 @@ marry_pay = marry.pays(bill, john)
 print(f'John pays: {john_pay}')
 print(f'Marry pays: {marry_pay}')
 print(f'Total: {marry_pay+john_pay}')
+pdf = PdfReport(filename='Report1')
+pdf.generate(flatmate1=john, flatmate2=marry, bill=bill)
