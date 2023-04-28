a8277699866696b52640439c0df1c0082bf88153
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2023-04-28 15:29:21 +0100

    reformat generated pdf for readability

--- App-2-Flatmates-Bill/main.py
+++ App-2-Flatmates-Bill/main.py
@@ -51,22 +51,33 @@ class PdfReport:
         # Add icon
         pdf.image('files/house.png', w=30, h=30)
 
-        # Add text
+        # Insert title
         pdf.set_font(family='Times', size=24, style='B')
         pdf.cell(
             w=0, h=80, txt='Flatmate Bill', align='C', ln=1
         )
+
+        # Insert period label and values
+        pdf.set_font(family='Times', size=18, style='B')
         pdf.cell(w=200, h=40, txt=f'Period: {bill.period}', ln=1)
+
+        # Flatmate1
+        pdf.set_font(family='Times', size=16)
         pdf.cell(
             w=175, h=40,
             txt=f'{flatmate1.name}: {flatmate1_pay}',
             ln=1
         )
+
+        # Flatmate2
         pdf.cell(
             w=175, h=40,
             txt=f'{flatmate2.name}: {flatmate2_pay}',
             ln=1
         )
+
+        # Period total
+        pdf.set_font(family='Times', size=16, style='B')
         pdf.cell(
             w=175, h=40,
             txt=f'Total: {round(float(bill.amount), 2)}',
