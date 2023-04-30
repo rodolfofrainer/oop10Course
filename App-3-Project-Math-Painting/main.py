36b1691a10c9c35cf65eb068a530f7f3f8a0bad6
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2023-04-30 17:18:06 +0100

    create square class

--- App-3-Project-Math-Painting/main.py
+++ App-3-Project-Math-Painting/main.py
@@ -18,3 +18,14 @@ class Rectangle:
 
     def draw(self, canvas):
         pass
+
+
+class Square:
+    def __init__(self, x, y, width, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.color = color
+
+    def draw(self, canvas):
+        pass
