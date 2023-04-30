ec6b6c688aab43ddcc8c0ae4f58146822d34f5cb
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2023-04-30 17:17:22 +0100

    create canvas class

---
+++ App-3-Project-Math-Painting/main.py
@@ -0,0 +1,8 @@
+class Canvas:
+    def __init__(self, width, height, color):
+        self.color = color
+        self.a = width
+        self.b = height
+
+    def make(self, imagepath):
+        pass
