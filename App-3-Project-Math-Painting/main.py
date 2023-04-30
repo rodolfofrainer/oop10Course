b3391bc03de146d871b20e6ad24c0558b5d63a75
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2023-04-30 17:32:46 +0100

    implement methods for classes

--- App-3-Project-Math-Painting/main.py
+++ App-3-Project-Math-Painting/main.py
@@ -1,11 +1,23 @@
+from PIL import Image
+import numpy as np
+
+
 class Canvas:
-    def __init__(self, width, height, color):
+    """Object where all shapes are drawn"""
+
+    def __init__(self, height, width, color):
         self.color = color
-        self.a = width
         self.b = height
+        self.a = width
+
+        # Create a 3d numpy of zeros
+        self.data = np.zeros((height, width, 3), dtype=np.uint8)
+        # Change [0,0,0] with user given values for color
+        self.data[:] = self.color
 
     def make(self, imagepath):
-        pass
+        img = Image.fromarray(self.data, 'RGB')
+        img.save(imagepath)
 
 
 class Rectangle:
@@ -17,7 +29,10 @@ class Rectangle:
         self.color = color
 
     def draw(self, canvas):
-        pass
+        canvas.data[
+            self.x: self.x+self.height,
+            self.y: self.y+self.width
+        ] = self.color
 
 
 class Square:
@@ -28,4 +43,15 @@ class Square:
         self.color = color
 
     def draw(self, canvas):
-        pass
+        canvas.data[
+            self.x: self.x+self.width,
+            self.y: self.y+self.width
+        ] = self.color
+
+
+canvas = Canvas(height=20, width=30, color=(255, 255, 255))
+r1 = Rectangle(x=1, y=6, height=7, width=10, color=(100, 200, 125))
+r1.draw(canvas)
+s1 = Square(x=1, y=3, width=3, color=(0, 100, 222))
+s1.draw(canvas)
+canvas.make('canvas.png')
