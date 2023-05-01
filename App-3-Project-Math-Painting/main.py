2a75c6d60213d32165fb00668ae12ca082acc197
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2023-05-01 11:49:21 +0100

    Create CLI

--- App-3-Project-Math-Painting/main.py
+++ App-3-Project-Math-Painting/main.py
@@ -1,10 +1,46 @@
 from canvas import Canvas
 from shapes import Square, Rectangle
 
+canvas_width = int(input('Enter canvas width:  '))
+canvas_height = int(input('Enter canvas height:  '))
+canvas_color = {'white': (255, 255, 255), 'black': (0, 0, 0)}
+color = input('Enter color (white or black): ').lower()
 
-canvas = Canvas(height=20, width=30, color=(255, 255, 255))
-r1 = Rectangle(x=1, y=6, height=7, width=10, color=(100, 200, 125))
-r1.draw(canvas)
-s1 = Square(x=1, y=3, width=3, color=(0, 100, 222))
-s1.draw(canvas)
-canvas.make('canvas.png')
+canvas = Canvas(height=canvas_height, width=canvas_width,
+                color=canvas_color[color])
+
+while True:
+    # defines shape type
+    shape_type = input('Shape type (square or rectangle): ')
+
+    if shape_type.lower() == 'rectangle':
+        # set rectangle values
+        rec_x = int(input('Enter rectangle x: '))
+        rec_y = int(input('Enter rectangle y: '))
+        rec_height = int(input('Enter rectangle height: '))
+        rec_width = int(input('Enter rectangle width: '))
+        rec_red = int(input('Enter rectangle amount red: '))
+        rec_green = int(input('Enter rectangle amount green: '))
+        rec_blue = int(input('Enter rectangle amount blue: '))
+
+        # create rectangle object
+        r1 = Rectangle(x=rec_x, y=rec_y, height=rec_height,
+                       width=rec_width, color=(rec_red, rec_green, rec_blue))
+        r1.draw(canvas)
+
+    if shape_type.lower() == 'square':
+        # set square values
+        sqr_x = int(input('Enter square x: '))
+        sqr_y = int(input('Enter square y: '))
+        sqr_side = int(input('Enter square\'s side of size: '))
+        sqr_red = int(input('Enter square amount red: '))
+        sqr_green = int(input('Enter square amount green: '))
+        sqr_blue = int(input('Enter square amount blue: '))
+
+        # create square object
+        s1 = Square(x=sqr_x, y=sqr_y, side=sqr_side,
+                    color=(sqr_side, sqr_green, sqr_blue))
+        s1.draw(canvas)
+
+    # Create canvas
+    canvas.make('canvas.png')
--- App-3-Project-Math-Painting/shapes.py
+++ App-3-Project-Math-Painting/shapes.py
@@ -14,14 +14,14 @@ class Rectangle:
 
 
 class Square:
-    def __init__(self, x, y, width, color):
+    def __init__(self, x, y, side, color):
         self.x = x
         self.y = y
-        self.width = width
+        self.side = side
         self.color = color
 
     def draw(self, canvas):
         canvas.data[
-            self.x: self.x+self.width,
-            self.y: self.y+self.width
+            self.x: self.x+self.side,
+            self.y: self.y+self.side
         ] = self.color
