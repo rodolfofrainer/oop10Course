5e4c7cab076dc60fee06cb37acca57d9ad59deb0
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2023-05-03 12:42:21 +0100

    app 4 boilerplate

--- App-4-Webcam-Photo-Sharer/main.py
+++ App-4-Webcam-Photo-Sharer/main.py
@@ -2,33 +2,27 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 
-import wikipedia
-import requests
-import os
-
 Builder.load_file('frontend.kv')
 
 
-class MainScreen(Screen):
-    def get_image_link(self):
-        # get user query from text input field
-        query = self.manager.current_screen.ids.user_query.text
-        # get wikipedia page and first image link
-        page = wikipedia.page(query, auto_suggest=False)
-        image_link = page.images[0]
-        return image_link
+class CameraScreen(Screen):
+    def start(self):
+        pass
 
-    def download_image(self):
-        # download image
-        req = requests.get(self.get_image_link())
-        filepath = 'images/image.jpeg'
-        with open(filepath, 'wb') as file:
-            file.write(req.content)
+    def stop(self):
+        pass
 
-    def set_image(self):
-        # set image in image widget
-        self.manager.current_screen.ids.img.source = self.download_image()
-        self.manager.current_screen.ids.img.reload()
+    def capture(self):
+        pass
+
+
+class ImageScreen(Screen):
+    pass
+
+
+class FileSharer:
+    def __init__(self, filepath):
+        self.filepath = filepath
 
 
 class RootWidget(ScreenManager):
